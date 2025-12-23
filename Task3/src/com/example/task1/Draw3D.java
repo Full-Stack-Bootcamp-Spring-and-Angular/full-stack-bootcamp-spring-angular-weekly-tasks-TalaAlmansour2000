36f4c08e1d43af7c165a82a162eb881cab00cfd8
Tala Almansour <Tala.Ahmad.Almansour@gmail.com>
2025-12-24 00:52:28 +0300

    Finish Task3

---
+++ Task3/src/com/example/task1/Draw3D.java
@@ -0,0 +1,11 @@
+package com.example.task1;
+
+import static java.lang.String.format;
+
+public class Draw3D {
+    public void draw(String shapeName) {
+        System.out.println(format("Drawing a 3d for %s", shapeName));
+    }
+
+
+}
---
+++ Task3/src/com/example/task2/DatabaseOperations.java
@@ -0,0 +1,14 @@
+package com.example.task2;
+public class DatabaseOperations {
+
+
+public void connectToDatabase() {
+    System.out.println(String.format("connection to database established……"));
+
+}
+
+    public void disconnectFromDatabase() {
+        System.out.println(String.format("connection to database closed ......"));
+    }
+
+}
