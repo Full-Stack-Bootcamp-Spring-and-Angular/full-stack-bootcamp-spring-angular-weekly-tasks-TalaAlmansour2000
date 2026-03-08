68c5cd9f9b53c0ec6b035bf76ae48cdb11974dd4
Tala Almansour <Tala.Ahmad.Almansour@gmail.com>
2026-03-08 15:47:00 +0300

    [Fix]: replace @RequestParma and use @ModelAttribute

--- Task5-UserProfileSystem/src/com/task5/userprofile/controller/Register.java
+++ Task5-UserProfileSystem/src/com/task5/userprofile/controller/Register.java
@@ -2,23 +2,26 @@ package com.task5.userprofile.controller;
 
 import com.task5.userprofile.dao.DatabaseOperations;
 import com.task5.userprofile.models.User;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
 @Controller
-
 public class Register {
 
     @Autowired
     private DatabaseOperations databaseOperations;
 
     @RequestMapping("/registerForm")
-    public String showFormRegister(){
+    public String showFormRegister(Model model){
+
+        model.addAttribute("user", new User());
         return ("userProfile/Register_Form");
     }
 
@@ -26,18 +29,8 @@ public class Register {
     @RequestMapping("/success")
 
     public String getData(
-            @RequestParam("firstName") String firstName,
-            @RequestParam("lastName") String lastName,
-            @RequestParam("email") String email,
-            @RequestParam("date") String dateofb, @RequestParam("city") String city ,
-            Model model) throws Exception {
+            @ModelAttribute("user") User user, Model model) throws Exception {
 
-        User user = new User();
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setEmail(email);
-        user.setDate(LocalDate.parse(dateofb));
-        user.setCity(city);
 
         databaseOperations.register(user);
         //java.sql.Date birthDate = java.sql.Date.valueOf(dateofb);
--- Task5-UserProfileSystem/src/com/task5/userprofile/models/User.java
+++ Task5-UserProfileSystem/src/com/task5/userprofile/models/User.java
@@ -1,18 +1,25 @@
 package com.task5.userprofile.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.context.annotation.Scope;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
     private String firstName;
     private  String lastName;
     private  String  email ;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private String city;
 
